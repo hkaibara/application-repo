d2530bc36e1e48a93cb2740f027693419869381f
hkaibara <hiroshi.kaibara.hk@gmail.com>
2024-05-14 22:56:50 +0800

    update image

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is a test flask app and i made some changes'
+    return 'Hello, World! This is a test flask app and i made some changes :D'
 
 if __name__ == '__main__':
     app.run(debug=True)
