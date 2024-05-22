a9becf654e54bf998e83dd215c9324fcfd9998ce
hkaibara <hiroshi.kaibara.hk@gmail.com>
2024-05-22 19:26:45 +0800

    reusable wf

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is a test flask app1'
+    return 'Hello, World! This is a test flask app'
 if __name__ == '__main__':
     app.run(debug=True)
