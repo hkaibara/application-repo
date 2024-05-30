eb29fedb1718a9ac2fb1e2c035628529dcbd77ec
hkaibara <hiroshi.kaibara.hk@gmail.com>
2024-05-30 13:23:01 +0800

    configmap

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
@@ -5,3 +6,5 @@ def hello_world():
     return 'Hello, World! This is a test flask app'
 if __name__ == '__main__':
     app.run(debug=True)
+    print(os.environ['env_var1'])
+    print(os.environ['env_var2'])
